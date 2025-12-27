66d08523ce71a75224e8154f506fecfc12cfe992
sndp bag <sndpbagg@gmail.com>
2025-12-27 07:26:15 +0530

    updated index.ts

---
+++ packages/instagram/src/index.ts
@@ -0,0 +1,8 @@
+// ============================================
+// packages/instagram/src/index.ts
+// ============================================
+export * from './client';
+export * from './media';
+export * from './engagement';
+export * from './insights';
+export * from './types';
